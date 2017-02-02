dc6b70f5bafc8131ab437fd3965045216b7abed6
amun33 <amunn33@gmail,com>
2017-02-02 13:02:45 -0600

    Removing unused command and README (use Wiki)

--- story/management/commands/exportSentStory.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from export_import.google_sheet_import import audit_stories
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        audit_stories()
